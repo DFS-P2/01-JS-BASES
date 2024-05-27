53d41d53b86c375995acbead07b0beae494717a3
webew <fillion.bertrand@gmail.com>
2024-05-27 11:11:36 +0200

    ex 15

--- 01-echauffement/js/ex15.js
+++ 01-echauffement/js/ex15.js
@@ -1,4 +1,7 @@
 // table de multiplication de 2
-for (let i = 1; i <= 10; i++) {
-	console.log(i + " x 2 = ", i * 2);
+for (let table = 1; table <= 10; table++) {
+	for (let i = 1; i <= 10; i++) {
+		// console.log(i + " x " + table + " = ", i * table);
+		console.log(`${i} x ${table} = ${i * table}`);
+	}
 }
